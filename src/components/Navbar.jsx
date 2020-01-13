f33623192aa81cab5d1c079edc23c2fa3d6f3d8f
Miguel <miguelpartida080497@gmail.com>
2020-01-13 11:41:46 -0600

    try to fix navbar for iphones

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,9 +9,9 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 class Navbar extends React.Component {
   render() {
     return (
-      <div className="w-full fixed h-20  bg-white shadow-xl flex flex-wrap items-center">
-        <div className="container mx-auto flex flex-wrap  lg:px-16 px-6 lg:py-0 py-2 bg-white">
-          <div className="flex-1 flex justify-between items-center">
+      <nav className="nav">
+        <div className="container nav-navbar">
+          <div className="flex flex-1 justify-between items-center">
             <Link to="/">
               <span className="font-bold text-4xl text-pink-600">ATENAS</span>
             </Link>
@@ -28,46 +28,44 @@ class Navbar extends React.Component {
             className="hidden lg:flex lg:items-center lg:w-auto w-full"
             id="menu"
           >
-            <nav>
-              <ul className="lg:flex items-center justify-between text-base pt-4 lg:pt-0">
-                <li className="text-xl">
-                  <Link
-                    to="/simulator"
-                    className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-pink-400"
-                  >
-                    Simulador
-                  </Link>
-                </li>
+            <ul className="lg:flex items-center justify-between text-base pt-8 lg:pt-0">
+              <li className="text-xl">
+                <Link
+                  to="/simulator"
+                  className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-pink-400"
+                >
+                  Simulador
+                </Link>
+              </li>
 
-                <li className="text-xl">
-                  <Link
-                    to=""
-                    className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-pink-400"
-                  >
-                    Quienes Somos
-                  </Link>
-                </li>
-                <li className="text-xl">
-                  <Link
-                    to="/help"
-                    className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-pink-400"
-                  >
-                    Ayuda
-                  </Link>
-                </li>
-                <li className="text-xl text-pink-400">
-                  <Link
-                    to="/login"
-                    className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-pink-400"
-                  >
-                    Inicia Sesión
-                  </Link>
-                </li>
-              </ul>
-            </nav>
+              <li className="text-xl">
+                <Link
+                  to=""
+                  className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-pink-400"
+                >
+                  Quienes Somos
+                </Link>
+              </li>
+              <li className="text-xl">
+                <Link
+                  to="/help"
+                  className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-pink-400"
+                >
+                  Ayuda
+                </Link>
+              </li>
+              <li className="text-xl text-pink-400">
+                <Link
+                  to="/login"
+                  className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-pink-400"
+                >
+                  Inicia Sesión
+                </Link>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
+      </nav>
     );
   }
 }
